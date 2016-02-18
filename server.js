dadec253165aeced921d3d499834be689abb902d
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-02-18 16:36:13 -0700

    mid problem but end of day

---
+++ server.js
@@ -0,0 +1,18 @@
+//look into this for script function:
+
+
+//var fs         = require("fs");
+//var Converter  = require("csvtojson").Converter;
+//var fileStream = fs.createReadStream("csv/gspc.csv");
+//
+//new converter instance 
+//var converter = new Converter({constructResult:true});
+//
+//end_parsed will be emitted once parsing finished 
+//converter.on("end_parsed", function (jsonObj) {
+//  var jsonfile = require('jsonfile');
+//  var file     = 'json/gspc.json';
+//  jsonfile.writeFile(file, jsonObj, function (err) { console.error(err); });});
+
+//read from file 
+//fileStream.pipe(converter);
